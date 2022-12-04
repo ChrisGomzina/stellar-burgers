57b279324647d0a84c14ea88feaeced46f0656d7
Christina Gomzina <chrisgoncharova@yandex.ru>
2022-12-04 21:21:56 +0300

    Adds AppHeader component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.module.css';
+
+import AppHeader from '../AppHeader/AppHeader.jsx';
+
+const App = () => {
+  return (
+    <AppHeader />
+  );
+}
+
+export default App;
