f0d977a3c7c2863cc54e00625cf5ab47beff99d4
Christina Gomzina <chrisgoncharova@yandex.ru>
2022-12-10 23:31:41 +0300

    Adds Modal and ModalOverlay components

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './BurgerConstructor.module.css';
 
 import CurrencyIcon from '../../images/CurrencyIcon.svg'; 
 
 import { ConstructorElement, DragIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import Modal from '../Modal/Modal.jsx';
 
 const BurgerConstructor = ({ data }) => {
   const bun = data.find(function (item) {return item.type === 'bun'});
   const ingredients = data.filter(function (item) {return item.type !== 'bun'});
+  const [isModelOpen, setIsModalOpen] = React.useState(false);
   
   return (
     <section className={`${styles.container} mt-20 pt-5 pb-5 pl-4`}>
@@ -28,10 +31,34 @@ const BurgerConstructor = ({ data }) => {
       <div className={`${styles.price_container} mt-10 mr-4`}>
         <span className={`text text_type_digits-medium mr-2`}>610</span>
         <img className={`mr-10`} src={CurrencyIcon} alt='Межгалактическая валюта.'/>
-        <Button htmlType="button" type="primary" size="large">Оформить заказ</Button>
+        <Button htmlType="button" type="primary" size="large" onClick={() => setIsModalOpen(true)}>Оформить заказ</Button>
       </div>
+      {isModelOpen && 
+        (<Modal>
+        
+        </Modal>
+      )}
     </section>
   )
 };
 
 export default BurgerConstructor;
+
+BurgerConstructor.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string,
+      name: PropTypes.string,
+      type: PropTypes.string,
+      proteins: PropTypes.number,
+      fat: PropTypes.number,
+      carbohydrates: PropTypes.number,
+      calories: PropTypes.number,
+      price: PropTypes.number,
+      image: PropTypes.string,
+      image_mobile: PropTypes.string,
+      image_large: PropTypes.string,
+      __v: PropTypes.number
+    }).isRequired
+  ).isRequired
+};
--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './BurgerIngredients.module.css';
 
@@ -78,3 +79,22 @@ const BurgerIngredients = ({ data }) => {
 };
 
 export default BurgerIngredients;
+
+BurgerIngredients.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string,
+      name: PropTypes.string,
+      type: PropTypes.string,
+      proteins: PropTypes.number,
+      fat: PropTypes.number,
+      carbohydrates: PropTypes.number,
+      calories: PropTypes.number,
+      price: PropTypes.number,
+      image: PropTypes.string,
+      image_mobile: PropTypes.string,
+      image_large: PropTypes.string,
+      __v: PropTypes.number
+    }).isRequired
+  ).isRequired
+};
